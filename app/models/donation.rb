cb03c1708ed94e962ecb9e3c4432e41b9ecced1c
Fraser <frasermince@gmail.com>
2015-01-22 16:20:45 -0600

    allow tests for updating donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,23 +1,41 @@
 class Donation < ActiveRecord::Base
   before_create :before_create
+  before_update :before_update, if: :amount_changed?
   belongs_to :parent, :class_name => 'Donation'
   has_many :children, :class_name => 'Donation', :foreign_key => 'parent_id'
   
   def before_create
-    traverse_upline self.parent
+    traverse_upline self.parent, 'create'
   end
-  
-  def update_downline_amounts(downline_amount)
-      self.downline_amount += downline_amount
+
+  def before_update
+    traverse_upline self.parent, 'update'
+  end
+
+  def traverse_upline(donation, action)
+    unless donation.nil?
+      donation.perform_update_action action, self.amount_was, self.amount
+      traverse_upline donation.parent, action
+    end
+  end
+
+  def perform_update_action(action, amount_was, amount)
+    if action == 'create'
+      add_downline_amount amount
+    elsif action == 'update'
+      replace_downline_amount amount_was, amount
+    end
+  end
+
+  def add_downline_amount(amount)
+      self.downline_amount += amount
       self.downline_count += 1
       self.save
   end
 
-  def traverse_upline(donation)
-    unless donation.nil?
-      donation.update_downline_amounts self.amount
-      traverse_upline donation.parent
-    end
+  def replace_downline_amount(old, new)
+    self.downline_amount -= old
+    self.downline_amount += new
+    self.save
   end
-
 end
--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe Donation, :type => :model do
   it { should have_many(:children) }
 
   describe 'creating a donation' do
-
-    it 'calculates the amount and count of one child on save' do
+    it 'calculates the amount and count of one child' do
       create_one_child
       expect_parent_downline_to_equal 1, 1
     end
@@ -24,12 +23,36 @@ RSpec.describe Donation, :type => :model do
       expect_parent_downline_to_equal 3, 12.00
       expect_child_downline_to_equal 2, 11.00
     end
+  end
+
+  describe 'updating a donation' do
+    it 'calculates the amount and count of one child' do
+      create_and_update_one_child
+      expect_parent_downline_to_equal 1, 5
+    end
+
+    it 'calculates even among multiple levels' do
+      create_and_update_grandchild
+      expect_parent_downline_to_equal 2, 7.00
+      expect_child_downline_to_equal 1, 6.00
+    end
+
+    it 'calculates with multiple children per level' do
+      create_and_update_second_grandchild
+      expect_parent_downline_to_equal 3, 13
+      expect_child_downline_to_equal 2, 12
+    end
 
   end
 
   def expect_child_downline_to_equal(count, amount)
     expect(@child_donation.reload.downline_count).to eq(count)
     expect(@child_donation.reload.downline_amount).to eq(amount)
+  end
+
+  def expect_parent_downline_to_equal(count, amount)
+    expect(@parent_donation.reload.downline_count).to eq(count)
+    expect(@parent_donation.reload.downline_amount).to eq(amount)
   end
 
   def create_parent
@@ -43,10 +66,20 @@ RSpec.describe Donation, :type => :model do
     @child_donation.save
   end
 
+  def create_and_update_one_child
+    create_one_child
+    @child_donation.update(amount: 5)
+  end
+
   def create_grandchild
     create_one_child
     @grandchild_donation = Donation.new(parent_id: 2, amount: 7.00, id:3)
     @grandchild_donation.save
+  end
+
+  def create_and_update_grandchild
+    create_grandchild
+    @grandchild_donation.update(amount: 6.00)
   end
 
   def create_second_grandchild
@@ -55,8 +88,8 @@ RSpec.describe Donation, :type => :model do
     @second_grandchild_donation.save
   end
 
-  def expect_parent_downline_to_equal(count, amount)
-    expect(@parent_donation.reload.downline_count).to eq(count)
-    expect(@parent_donation.reload.downline_amount).to eq(amount)
+  def create_and_update_second_grandchild
+    create_second_grandchild
+    @second_grandchild_donation.update(amount: 5.00)
   end
 end
