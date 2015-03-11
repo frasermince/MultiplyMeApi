492d8b05d242ba03feb1edebe4b421e2d6acff58
Fraser <frasermince@gmail.com>
2015-03-11 15:05:13 -0500

    add comments to donation model

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,11 +1,14 @@
+# Donation is used to track donations created by the user
+# Includes two concerns
 class Donation < ActiveRecord::Base
-  include Traversable
   # tracks a user's downline
+  include Traversable
+  # tracks the amount of money donated
+  # automatically charges for a donation when it has three children
   include Pledgeable
-  #tracks the amount of monay donated
   belongs_to :user
   belongs_to :organization
+  # amount is a integer in cents
   validates :amount, :downline_count, :downline_amount,
             :organization_id, :user_id, presence: true
-  # amount is a integer in cents
 end
