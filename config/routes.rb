9c9c1c9a9651cab447161e063dce71a13a63348e
Fraser <frasermince@gmail.com>
2015-03-18 17:58:08 -0500

    add amala subdomain for angular app

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  constraints :subdomain => 'amala' do
+    namespace :amala, path: nil, defaults: {format: 'json'}  do
+      get '/', to: redirect('/')
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
