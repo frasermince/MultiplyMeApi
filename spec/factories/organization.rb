c5dc39a44e349a623d8a39d6fdcba8273638fd1b
Fraser <frasermince@gmail.com>
2015-03-24 22:06:38 -0500

    update stripe_access_token

--- spec/factories/organization.rb
+++ spec/factories/organization.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :organization do
-    stripe_access_token 'sk_test_95OpHNSBDukHcgcLfxMiYKJF'
+    stripe_access_token 'sk_test_zdPjM9RBhcvgd6Csz8Q6lTUR'
     name 'test'
     factory :invalid_organization do
       stripe_access_token '12345'
