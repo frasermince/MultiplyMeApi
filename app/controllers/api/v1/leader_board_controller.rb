2c50adfb635fcbc34f6f1f45d2738caa4c66970a
Fraser <frasermince@gmail.com>
2015-05-07 00:14:56 -0500

    have get_leaders get top users instead of donations

--- app/controllers/api/v1/leader_board_controller.rb
+++ app/controllers/api/v1/leader_board_controller.rb
@@ -5,14 +5,15 @@ module Api
       def index
         limit = params[:limit].nil? ? 10 : params[:limit]
         @leaders = get_leaders limit
-        @leaders.map
         render json: {leaders: @leaders}, status: :ok
       end
 
       private
 
       def get_leaders limit
-        Donation.joins(:user).select(:id, 'users.name as name', 'users.email as email', 'personal_impact + network_impact as contribution').order('contribution DESC').limit(limit)
+        User.order('personal_impact + network_impact DESC')
+          .limit(limit)
+          .select(:id, 'users.name as name', 'users.email as email', 'personal_impact + network_impact as contribution')
       end
 
     end
