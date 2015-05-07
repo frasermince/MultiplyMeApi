7c8bfdd43b72cda2c75c605f344685611f33e8b2
Fraser <frasermince@gmail.com>
2015-05-07 01:48:11 -0500

    have json be an object instead of an array

--- app/controllers/api/v1/user_donations_controller.rb
+++ app/controllers/api/v1/user_donations_controller.rb
@@ -3,7 +3,7 @@ module Api
     class UserDonationsController < ApplicationController
       def show
         user = User.find(params[:id])
-        render json: user.donations.map{|donation| donation.id}, status: :ok
+        render json: {donation_ids: user.donations.map{|donation| donation.id}}, status: :ok
       end
     end
   end
