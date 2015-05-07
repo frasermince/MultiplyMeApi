038fe706c14f3b4ad625933b77780cde4deb419a
Fraser <frasermince@gmail.com>
2015-05-07 01:30:55 -0500

    include schema changes in repo

--- db/schema.rb
+++ db/schema.rb
@@ -11,43 +11,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413214051) do
+ActiveRecord::Schema.define(version: 20150507060732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "donations", force: true do |t|
+  create_table "donations", force: :cascade do |t|
     t.integer  "parent_id"
     t.integer  "amount"
-    t.integer  "downline_count",  default: 0
-    t.float    "downline_amount", default: 0.0
+    t.integer  "downline_count",              default: 0
+    t.float    "downline_amount",             default: 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "organization_id"
     t.integer  "user_id"
-    t.boolean  "is_subscription", default: true
-    t.boolean  "is_challenged",   default: true
-    t.boolean  "is_paid",         default: false
-    t.string   "stripe_id"
+    t.boolean  "is_subscription",             default: true
+    t.boolean  "is_challenged",               default: true
+    t.boolean  "is_paid",                     default: false
+    t.string   "stripe_id",       limit: 255
   end
 
   add_index "donations", ["organization_id"], name: "index_donations_on_organization_id", using: :btree
   add_index "donations", ["user_id"], name: "index_donations_on_user_id", using: :btree
 
-  create_table "organizations", force: true do |t|
-    t.string   "name"
-    t.string   "stripe_access_token"
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name",                limit: 255
+    t.string   "stripe_access_token", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "stripe_id"
-    t.integer  "donation_amount",     default: 0
-    t.integer  "donation_count",      default: 0
+    t.string   "stripe_id",           limit: 255
   end
 
-  create_table "organizations_users", force: true do |t|
+  create_table "organizations_users", force: :cascade do |t|
     t.integer  "organization_id"
     t.integer  "user_id"
-    t.string   "stripe_id"
+    t.string   "stripe_id",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -55,33 +53,33 @@ ActiveRecord::Schema.define(version: 20150413214051) do
   add_index "organizations_users", ["organization_id"], name: "index_organizations_users_on_organization_id", using: :btree
   add_index "organizations_users", ["user_id"], name: "index_organizations_users_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "provider",                            null: false
-    t.string   "uid",                    default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",               limit: 255,              null: false
+    t.string   "uid",                    limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.string   "nickname"
-    t.string   "image"
-    t.string   "email"
+    t.string   "unconfirmed_email",      limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "nickname",               limit: 255
+    t.string   "image",                  limit: 255
+    t.string   "email",                  limit: 255
     t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "stripe_id"
-    t.integer  "network_impact",         default: 0
-    t.integer  "personal_impact",        default: 0
-    t.integer  "recurring_amount",       default: 0
+    t.string   "stripe_id",              limit: 255
+    t.integer  "network_impact",                     default: 0
+    t.integer  "personal_impact",                    default: 0
+    t.integer  "recurring_amount",                   default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
