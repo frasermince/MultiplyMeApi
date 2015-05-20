792c4e8f53c06267d7cbeda8d5242010115e0cff
Fraser <frasermince@gmail.com>
2015-05-19 22:54:02 -0500

    remove unused methods

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -2,20 +2,6 @@ module Overrides
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
     skip_before_action :authenticate_user!
 
-    def redirect_callbacks
-      # derive target redirect route from 'resource_class' param, which was set
-      # before authentication.
-      devise_mapping = request.env['omniauth.params']['resource_class'].underscore.to_sym
-      redirect_route = "#{Devise.mappings[devise_mapping].as_json["path"]}/#{params[:provider]}/callback"
-
-      # preserve omniauth info for success route. ignore 'extra' in twitter
-      # auth response to avoid CookieOverflow.
-      session['dta.omniauth.auth'] = request.env['omniauth.auth'].except('extra')
-      session['dta.omniauth.params'] = request.env['omniauth.params']
-
-      redirect_to redirect_route
-    end
-
     def omniauth_success
       if params[:provider] == 'stripe_connect'
         organization = Organization.find params[:state]
@@ -23,10 +9,6 @@ module Overrides
       else
         super
       end
-    end
-
-    def resource_class
-      User
     end
   end
 end
