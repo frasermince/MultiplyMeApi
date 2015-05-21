122785f9f905b94093956064c65b73a01d9eedc1
Fraser <frasermince@gmail.com>
2015-05-21 16:34:03 -0500

    add default name for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'api.multiplyme.in'}
+  config.action_mailer.default_options = {from: 'MultiplyMe'}
 
   #config.action_mailer.delivery_method = :test
 
