804173d23e5d242ddcb3cb9d1e7ce8d06fe31f20
Fraser <frasermince@gmail.com>
2015-05-22 16:20:13 -0500

    fix issue where amounts were coming back as 0

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class OrganizationsController < ApplicationController
       def show
-        @organization = Organization.select('name, campaign_end_date').find params[:id]
+        @organization = Organization.select('name, campaign_end_date, id').find params[:id]
         render json: {organization: @organization}, status: :ok, methods: [:donation_count, :donation_amount]
       end
     end
