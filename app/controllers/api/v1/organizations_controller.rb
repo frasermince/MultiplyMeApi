8685eefa5f52be866b7b607a3cf976cb7c2ef610
Fraser <frasermince@gmail.com>
2015-05-22 13:56:24 -0500

    do not display all organization fields

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class OrganizationsController < ApplicationController
       def show
-        @organization = Organization.find params[:id]
+        @organization = Organization.select('name, campaign_end_date').find params[:id]
         render json: {organization: @organization}, status: :ok, methods: [:donation_count, :donation_amount]
       end
     end
