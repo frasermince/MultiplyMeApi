eda774cd38cbd08b6b65bfe6f7e4236029782129
Fraser <frasermince@gmail.com>
2015-06-18 12:52:32 -0500

    remove superfluous code

--- app/models/concerns/traversable.rb
+++ app/models/concerns/traversable.rb
@@ -3,25 +3,8 @@ module Traversable
   extend ActiveSupport::Concern
 
   included do
-    before_create :before_create
-    before_update :before_amount_update, if: :amount_changed?
-    before_destroy :before_destroy
     belongs_to :parent, :class_name => 'Donation'
     has_many :children, :class_name => 'Donation', :foreign_key => 'parent_id'
-  end
-
-  def before_create
-    traverse_upline self.parent, 'create'
-  end
-
-  def before_destroy
-    traverse_upline self.parent, 'destroy'
-  end
-
-  def before_amount_update
-    if self.amount_was.present?
-      traverse_upline self.parent, 'update'
-    end
   end
 
   def traverse_downline(set)
@@ -30,44 +13,6 @@ module Traversable
       set = set | child.traverse_downline(set)
     end
     set
-  end
-
-  def traverse_upline(donation, action)
-    unless donation.nil?
-      if self.amount_was.present? && self.is_subscription
-        amount_was = self.amount_was * 12
-      end
-      donation.perform_update_action action, amount_was, self.yearly_amount, self.user_cycles?
-        traverse_upline donation.parent, action
-    end
-  end
-
-  def perform_update_action(action, amount_was, amount, cycles)
-    if action == 'create'
-      add_downline_amount amount
-    elsif action == 'update'
-      replace_downline_amount amount_was, amount
-    elsif action == 'destroy'
-      reduce_downline amount
-    end
-  end
-
-  def add_downline_amount(amount)
-    self.downline_amount += amount
-    self.downline_count += 1
-    self.save
-  end
-
-  def replace_downline_amount(old, new)
-    self.downline_amount -= old
-    self.downline_amount += new
-    self.save
-  end
-
-  def reduce_downline(amount)
-    self.downline_amount -= amount
-    self.downline_count -= 1
-    self.save
   end
 
   def one_grandchild
--- spec/models/concerns/traversable_spec.rb
+++ spec/models/concerns/traversable_spec.rb
@@ -8,68 +8,6 @@ end
 #uses donation as the model that uses traversable
 #currently this is the only model that uses it
 RSpec.describe Traversable do
-  describe 'creating a donation' do
-    it 'calculates the amount and count of one child' do
-      create_one_child
-      expect_parent_downline_to_equal 1, child_amount
-    end
-
-    it 'calculates even among multiple levels' do
-      create_grandchild
-      expect_parent_downline_to_equal 2, grandchild_amount + child_amount
-      expect_child_downline_to_equal 1, grandchild_amount
-    end
-
-    #add second grandchild
-    it 'calculates with multiple children per level' do
-      create_second_grandchild
-      expect_parent_downline_to_equal 3, child_amount + grandchild_amount + second_grandchild_amount
-      expect_child_downline_to_equal 2, grandchild_amount + second_grandchild_amount
-    end
-  end
-
-  describe 'updating a donation' do
-    it 'calculates the amount and count of one child' do
-      create_and_update_one_child
-      expect_parent_downline_to_equal 1, updated_child_amount
-    end
-
-    it 'calculates even among multiple levels' do
-      create_and_update_grandchild
-      expect_parent_downline_to_equal 2, child_amount + updated_grandchild_amount
-      expect_child_downline_to_equal 1, updated_grandchild_amount
-    end
-
-    it 'calculates with multiple children per level' do
-      create_and_update_second_grandchild
-      expect_parent_downline_to_equal 3, child_amount + grandchild_amount + updated_second_grandchild_amount
-      expect_child_downline_to_equal 2, grandchild_amount + updated_second_grandchild_amount
-    end
-
-  end
-
-  describe 'destroying a donation' do
-    it 'calculates the amount and count of one child' do
-      create_one_child
-      @child_donation.destroy
-      expect_parent_downline_to_equal 0, 0
-    end
-
-    it 'calculate even among multiple levels' do
-      create_grandchild
-      @grandchild_donation.destroy
-      expect_parent_downline_to_equal 1, child_amount
-      expect_child_downline_to_equal 0, 0
-    end
-
-    it 'calculates with multiple children per level' do
-      create_second_grandchild
-      @second_grandchild_donation.destroy
-      expect_parent_downline_to_equal 2, child_amount + grandchild_amount
-      expect_child_downline_to_equal 1, grandchild_amount
-    end
-
-  end
 
   describe '#one_grandchild' do
     context 'there is one grandchild' do
@@ -84,17 +22,5 @@ RSpec.describe Traversable do
         expect(@parent_donation.one_grandchild).to eq(false)
       end
     end
-  end
-
-  def expect_child_downline_to_equal(count, amount)
-    donation = Donation.find attributes_for(:child)[:id]
-    expect(donation.downline_count).to eq(count)
-    expect(donation.downline_amount).to eq(amount)
-  end
-
-  def expect_parent_downline_to_equal(count, amount)
-    donation = Donation.find attributes_for(:parent)[:id]
-    expect(donation.downline_count).to eq(count)
-    expect(donation.downline_amount).to eq(amount)
   end
 end
