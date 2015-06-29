2d5681e1bbef8e1c91874c08fe7be64dd0c176cd
Fraser <frasermince@gmail.com>
2015-06-29 16:01:49 -0500

    add method to get id to referral service

--- app/services/referral_code_service.rb
+++ app/services/referral_code_service.rb
@@ -8,7 +8,20 @@ class ReferralCodeService
     @donation.update_attribute('referral_code', referral_code)
   end
 
-  def self.find_by_code(code)
-    Donation.where(referral_code: code).first
+  def self.find_donation_by_code(code)
+    if code.present?
+      Donation.where(referral_code: code).first
+    else
+      nil
+    end
+  end
+
+  def self.find_id_by_code(code)
+    donation = find_donation_by_code code
+    if donation.present?
+      donation.id
+    else
+      nil
+    end
   end
 end
--- spec/services/referral_code_service_spec.rb
+++ spec/services/referral_code_service_spec.rb
@@ -10,10 +10,17 @@ RSpec.describe ReferralCodeService do
     end
   end
 
-  describe '#find_by_code' do
+  describe '#find_donation_by_code' do
     it 'finds based on a given code' do
       create(:referral_donation)
-      expect(ReferralCodeService.find_by_code('FraserMince97')).to be
+      expect(ReferralCodeService.find_donation_by_code('FraserMince97')).to be
+    end
+  end
+
+  describe '#find_id_by_code' do
+    it 'finds an id based on a given code' do
+      create(:referral_donation)
+      expect(ReferralCodeService.find_id_by_code('FraserMince97')).to be
     end
   end
 end
