862b8010239281c59ebf574e4f8962efb7abec31
Fraser <frasermince@gmail.com>
2015-07-08 10:13:37 -0600

    Refactor payment model to use new stripe_client constructor. Now purchase uses several small private methods and uses the new constructor.

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -1,24 +1,36 @@
 class PaymentService
-  def initialize(donation)
+  def initialize(donation, organization)
     @donation = donation
-    @stripe_client = StripeClient.new
+    @stripe_client = StripeClient.new(organization)
   end
 
   def purchase
     if @donation.is_paid
       false
     else
-      customer = get_stripe_user
-      @donation.is_subscription ? @stripe_client.create_subscription(customer) : @stripe_client.create_charge(customer)
-      @donation.update(is_paid: true)
-
+      pay_by_stripe_user
     end
   end
 
   private
+  def pay_by_stripe_user
+    customer = get_stripe_user
+    subscribe_or_donate(customer)
+    @donation.update(is_paid: true)
+    true
+  end
+
   def get_stripe_user
-    Stripe.api_key = Rails.application.secrets.stripe_secret_key
-    organizations_user = OrganizationsUser.find_or_create(@donation.organization_id, @donation.user_id)
-    organizations_user.get_stripe_user
+    OrganizationsUser
+      .find_or_create(@donation.organization_id, @donation.user_id)
+      .get_stripe_user
+  end
+
+  def subscribe_or_donate(customer)
+    if @donation.is_subscription
+      @stripe_client.create_subscription(@donation, customer)
+    else
+      @stripe_client.create_charge(@donation, customer)
+    end
   end
 end
