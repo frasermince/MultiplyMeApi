907d68fd9d4759d367e016eeb82b488052543b59
Fraser <frasermince@gmail.com>
2015-07-08 10:43:18 -0600

    Remove unnecessary methods from stripe user service. Some methods were no longer needed in stripe user service so they were removed.

--- spec/services/stripe_user_service_spec.rb
+++ spec/services/stripe_user_service_spec.rb
@@ -14,23 +14,20 @@ RSpec.describe StripeUserService do
 
   describe '#save_stripe_user' do
     it 'calls create_stripe_user' do
-      allow(@stripe_user_service).to receive(:create_stripe_user).and_return(1)
+      customer = create_stripe_user
+      allow(@stripe_client).to receive(:create_stripe_user).and_return(customer)
       @stripe_user_service.save_stripe_user(valid_stripe_params)
-      expect(@stripe_user_service).to have_received(:create_stripe_user)
-      expect(@user.reload.stripe_id).to eq("1")
+      expect(@stripe_client).to have_received(:create_stripe_user)
+      expect(@user.reload.stripe_id).to eq(customer.id)
     end
   end
 
   describe '#add_credit_card' do
     it 'has stripe_id and thus can create card' do
+      customer = create_stripe_user
+      @user.update_attribute('stripe_id', customer.id)
       allow(@stripe_client).to receive(:create_credit_card).and_return(true)
-      VCR.use_cassette('save_stripe_user') do
-        @stripe_user_service.save_stripe_user(valid_stripe_params)
-      end
-
-      result = VCR.use_cassette('add_credit_card') do
-        @stripe_user_service.add_credit_card create_token
-      end
+      result = @stripe_user_service.add_credit_card create_token
       expect(result).to eq(true)
     end
 
