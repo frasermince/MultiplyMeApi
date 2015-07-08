3623d010569685d1d7cbc3d1ace34f783780775b
Fraser <frasermince@gmail.com>
2015-07-08 11:54:47 -0600

    Have decorator work with new payment service. While there are still some failing tests here I have at least removed some no longer used methods from decorator and changed the constructors for various services.

--- app/decorators/donation_decorator.rb
+++ app/decorators/donation_decorator.rb
@@ -5,12 +5,6 @@ class DonationDecorator
     @donation.parent_id = ReferralCodeService.find_id_by_code(referrer)
     @subscribe_to_mail = subscribe_to_mail
     @card_params = card_params
-    @parent_payment_service = PaymentService.new @donation.parent
-    @donation_payment_service = PaymentService.new @donation
-    @mailing_list_service = MailingListService.new(@donation.user)
-    @stripe_user_service = StripeUserService.new(@donation.user)
-    @errors = []
-    @notification_service = NotificationService.new @donation
   end
 
   def donation
@@ -20,33 +14,18 @@ class DonationDecorator
   def save
     @donation.transaction do
       contains_card &&
-        @stripe_user_service.save_stripe_user(@card_params) &&
-        @donation.save &&
-        call_payment_service &&
-        @notification_service.send_mail &&
+        StripeUserService.new(@donation.user).save_stripe_user(@card_params)
+        @donation.save!
+        call_payment_service
+        @notification_service.send_mail
         subscribe_to_mail
     end
-  end
-
-  def errors
-    errors = @errors
-    # include errors from saving the stripe user
-    unless contains_card
-      errors.push 'card not provided'
-    end
-    errors.concat @mailing_list_service.errors
-    errors.concat @donation.errors.full_messages
-    errors.concat @parent_payment_service.errors
-    errors.concat @donation_payment_service.errors
-    errors.concat @stripe_user_service.errors
   end
 
   private
   def subscribe_to_mail
     if @subscribe_to_mail
-      @mailing_list_service.mailing_subscribe('c8e3eb0f3a')
-    else
-      true
+      MailingListService.new(@donation.user).mailing_subscribe('c8e3eb0f3a')
     end
   end
 
@@ -55,17 +34,21 @@ class DonationDecorator
   end
 
   def call_payment_service
-    parent_result = true
-    child_result = true
     parent = @donation.reload.parent
-    unless @donation.is_challenged
-      child_result = @donation_payment_service.purchase
-    end
     policy = CompletedChallengePolicy.new parent
-    if parent.present? && policy.challenge_completed?
-      parent_result = @parent_payment_service.purchase
+    donation_purchase && parent_purchase(parent, policy)
+  end
+
+  def donation_purchase
+    unless @donation.is_challenged
+      PaymentService.new(@donation, @donation.organization).purchase
     end
-    parent_result && child_result
+  end
+
+  def parent_purchase(parent, policy)
+    if parent.present? && policy.challenge_completed?
+      PaymentService.new(parent, parent.organization).purchase
+    end
   end
 
 end
