9d7961482fe7b0c88d5447421b6df5682093f39f
Fraser <frasermince@gmail.com>
2015-07-08 10:43:18 -0600

    Remove unnecessary methods from stripe user service. Some methods were no longer needed in stripe user service so they were removed.

--- app/services/stripe_user_service.rb
+++ app/services/stripe_user_service.rb
@@ -9,25 +9,16 @@ class StripeUserService
     if @user.stripe_id.present?
       result = add_credit_card(params[:token])
     else
-      result = create_stripe_user params
-      @user.update_attribute('stripe_id', result) if result != false
+      result = @stripe_client.create_stripe_user(params)
+      @user.update_attribute('stripe_id', result.id)
     end
     result
-  end
-
-  def create_stripe_user(params)
-    result = @stripe_client.create_stripe_user(params)
-    result != false ? result.id : false
   end
 
   def add_credit_card(token)
     Stripe.api_key = Rails.application.secrets.stripe_secret_key
     if @user.stripe_id.present?
       @stripe_client.create_credit_card token, @user
-      true
-    else
-      #@errors.push 'customer is not present'
-      return false
     end
   end
 end
