3db6f4be3f792e99c933669b6f6e3d0b72d870ff
Fraser <frasermince@gmail.com>
2015-07-10 11:45:09 -0600

    Rewrite payment service to accept an array of strategies and have a function to call pay on every one of them.

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -1,36 +1,9 @@
 class PaymentService
-  def initialize(donation, organization)
-    @donation = donation
-    @stripe_client = StripeClient.new(organization)
+  def initialize(strategy_array)
+    @strategy_array = strategy_array
   end
 
-  def purchase
-    if @donation.is_paid
-      false
-    else
-      pay_by_stripe_user
-    end
-  end
-
-  private
-  def pay_by_stripe_user
-    customer = get_stripe_user
-    subscribe_or_donate(customer)
-    @donation.update(is_paid: true)
-    true
-  end
-
-  def get_stripe_user
-    OrganizationsUser
-      .find_or_create(@donation.organization_id, @donation.user_id)
-      .get_stripe_user
-  end
-
-  def subscribe_or_donate(customer)
-    if @donation.is_subscription
-      @stripe_client.create_subscription(@donation, customer)
-    else
-      @stripe_client.create_charge(@donation, customer)
-    end
+  def pay
+    @strategy_array.each{|strategy| strategy.pay}
   end
 end
--- spec/services/payment_service_spec.rb
+++ spec/services/payment_service_spec.rb
@@ -1,82 +1,17 @@
 require 'rails_helper'
 
-RSpec.configure do |c|
-  c.include StripeHelpers
-end
-
 RSpec.describe PaymentService do
-
-  before(:each) do
-    @stripe_client = StripeClient.new
-    allow(StripeClient).to receive(:new).and_return(@stripe_client)
-  end
-
-  describe '#purchase' do
-    context 'donation is not already paid' do
-
-      context 'and donation is a subscription' do
-        it 'calls create_subscription' do
-          donation = create(:donation)
-          organization = create(:organization)
-          organizations_user = create(:organizations_user)
-          donation.is_subscription = true
-          customer = create_stripe_user
-          allow(OrganizationsUser)
-            .to receive(:find_or_create)
-            .and_return(organizations_user)
-          allow(organizations_user)
-            .to receive(:get_stripe_user)
-            .and_return(customer)
-
-          allow(@stripe_client).to receive(:create_subscription)
-          payment_service = PaymentService.new donation, organization
-
-          expect(payment_service.purchase).to eq(true)
-          expect(@stripe_client).to have_received(:create_subscription)
-        end
+  describe '#pay' do
+    it 'calls pay on each strategy' do
+      strategies = [double('first_strategy'), double('second_strategy')]
+      strategies.each do |strategy|
+        allow(strategy)
+          .to receive(:pay)
+        expect(strategy)
+          .to receive(:pay)
       end
-
-      context 'and donation is not a subscription' do
-        it 'calls create_charge' do
-          donation = create(:donation)
-          donation.is_subscription = false
-          organization = create(:organization)
-          organizations_user = create(:organizations_user)
-          payment_service = PaymentService.new donation, organization
-          customer = create_stripe_user
-          allow(OrganizationsUser)
-            .to receive(:find_or_create)
-            .and_return(organizations_user)
-          allow(organizations_user)
-            .to receive(:get_stripe_user)
-            .and_return(customer)
-          allow(@stripe_client).to receive(:create_charge)
-
-          expect(payment_service.purchase).to eq(true)
-          expect(@stripe_client).to have_received(:create_charge).with(any_args, customer)
-        end
-      end
-
-      context 'payment has already been made' do
-        it 'returns a status of success' do
-          donation = build_stubbed(:donation)
-          donation.is_paid = true
-          organization = create(:organization)
-          payment_service = PaymentService.new donation, organization
-          expect(payment_service.purchase).to be_falsey
-        end
-      end
+      payment_service = PaymentService.new strategies
+      payment_service.pay
     end
-
-    context 'purchase was previously made' do
-      it 'returns a failed status' do
-        donation = create(:donation)
-        organization = create(:organization)
-        donation.is_paid = true
-        payment_service = PaymentService.new donation, organization
-        expect(payment_service.purchase).to eq(false)
-      end
-    end
-
   end
 end
