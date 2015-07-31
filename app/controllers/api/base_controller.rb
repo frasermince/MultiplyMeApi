6c2fe690c1fd14f5ea8115f0bfac9a4e658985e2
Fraser <frasermince@gmail.com>
2015-07-30 21:27:21 -0500

    Do not over organization_id parameter.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class BaseController < ApplicationController
-    before_action :set_organization
+    #before_action :set_organization
     protected
     def set_organization
       params[:organization_id] = 2
