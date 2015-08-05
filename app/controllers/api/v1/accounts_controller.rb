930afda1a4c3ed61f5db675f285dafcb073c7590
Fraser <frasermince@gmail.com>
2015-08-05 01:31:29 -0500

    Add referral code to accounts page.

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -6,17 +6,19 @@ module Api
         @organization_id = params[:organization_id]
         personal_impact = user.personal_impact @organization_id
         network_impact = user.network_impact @organization_id
-        render json: json_response(user, personal_impact, network_impact), status: :ok
+        referral_code = user.donations.present? ? user.donations.last.referral_code : nil
+        render json: json_response(user, personal_impact, network_impact, referral_code), status: :ok
       end
       private
-      def json_response(user, personal_impact, network_impact)
+      def json_response(user, personal_impact, network_impact, referral_code)
         {
           personal_impact: personal_impact,
           network_impact: network_impact,
           total_impact: personal_impact + network_impact,
           recurring_amount: user.recurring_amount(@organization_id),
           only_recurring: user.only_recurring(@organization_id),
-          all_cancelled: user.all_cancelled?(@organization_id)
+          all_cancelled: user.all_cancelled?(@organization_id),
+          referral_code: referral_code
         }
       end
     end
