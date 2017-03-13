087f284e466f2075f1f6ed0fcefd71d7618ab5ad
Fraser Mince <frasermince@gmail.com>
2017-03-12 21:06:01 -0600

    Do not always record create stripe token.

--- spec/support/stripe_helpers.rb
+++ spec/support/stripe_helpers.rb
@@ -16,7 +16,7 @@ module StripeHelpers
 
   def create_token_object(card=4242424242424242)
     Stripe.api_key = Rails.application.secrets.stripe_secret_key
-    VCR.use_cassette('create_stripe_token', :record => :all) do
+    VCR.use_cassette('create_stripe_token') do
       Stripe::Token.create(
         card: {
           :number => card,
